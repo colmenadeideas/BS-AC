b68ac53fca74e9aceedb70144297a2ab681842aa
Melanie Marval <mmarval@besign.com.ve>
2019-11-12 10:56:13 -0400

    modificacion de css login

--- frontend/src/components/Devices/DeviceInstall.js
+++ frontend/src/components/Devices/DeviceInstall.js
@@ -9,7 +9,7 @@ const DeviceInstall = ({history}) => {
     }
     return ( 
         <div className="row mt-5">
-            <div className="col-lg-4 d-flex flex-column justify-content-center text-right">
+            <div className="col-lg-3 d-flex flex-column justify-content-center text-right">
                 <h3>Instala tu Dispositivo</h3>
                 <ol className="list">
                     <li className="list-items">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
@@ -21,9 +21,9 @@ const DeviceInstall = ({history}) => {
                 <h1 className="mb-5 text-center col-sm-12">Instala tu Dispositivo</h1>
                 <img src="../images/img4.jpg" className="w-75" alt="mobile"/>
             </div>
-            <div className="col-lg-2 d-flex flex-column justify-content-center">
+            <div className="col-lg-3 d-flex flex-column justify-content-center">
                 <h3>Empecemos!</h3>
-                <button className="btn btn-dark redondeado mt-3" onClick="handleClick">Enviar Codigo</button>
+                <button className="btn btn-dark redondeado mt-3" onClick={handleClick}>Enviar Codigo</button>
             </div>
         </div>
      );
