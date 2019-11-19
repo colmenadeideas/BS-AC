df430e068d7a041882c496ac6db563f349fd1ee6
Alejandra Ortega <aortega@besign.com>
2019-11-19 16:10:04 -0400

    navbar
    Notificacion de dispositivo de registro

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Navbar = () => {
 return (
 <div className="header-dashboard">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light ">
         <Link className="navbar-brand" to="/">Logo</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,31 +15,44 @@ return (
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/access/dashboard">Dashboard <span className="sr-only">(current)</span></Link>
+            <li className="nav-item">
+                    <Link className="nav-link" to="/access/dashboard">Dashboard</Link>
                 </li>
+                
                 <li className="nav-item">
                     <Link className="nav-link" to="/">Link</Link>
                 </li>
-                <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </Link>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <Link className="dropdown-item" to="/">Action</Link>
-                        <Link className="dropdown-item" to="/">Another action</Link>
-                        <div className="dropdown-divider"></div>
-                        <Link className="dropdown-item" to="/">Something else here</Link>
-                    </div>
-                </li>
+                
                 <li className="nav-item">
-                    <Link className="nav-link disabled" to="/" tabindex="-1" aria-disabled="true">Disabled</Link>
+                    <Link className="nav-link" to="/">Link</Link>
                 </li>
+
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Link</Link>
+                </li>
+                
             </ul>
             <div className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+               
+                <ul className="navbar-nav mr-auto">
+                    
+                    <li className="nav-item dropdown alert-active">
+                        <Link className="nav-link dropdown-toggle text-white" to="/" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+                            <div className="alert-bell">
+                                <div className="alert-note text-white px-2">1</div><FontAwesomeIcon icon="bell"  style={{ 'color': 'white' }} />    
+                            </div>
+                            
+                        </Link>
+                        <div className="dropdown-menu " aria-labelledby="navbarDropdown">
+                            <Link className="dropdown-item alert-success" to="/">Dispositivo registrado con exito</Link>
+                            <Link className="dropdown-item" to="/">Another action</Link>
+                            <div className="dropdown-divider"></div>
+                            <Link className="dropdown-item" to="/">Something else here</Link>
+                        </div>
+                    </li>
+                    
+                </ul>
             </div>
         </div>
 
