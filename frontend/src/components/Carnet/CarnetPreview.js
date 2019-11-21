49d612d25586bd5cb1ff8e2f66a023d301d2aabd
Carlos Moreno <cmoreno@besign.com.ve>
2019-11-21 09:50:41 -0400

    Update CarnetPreview.js

--- frontend/src/components/Carnet/CarnetPreview.js
+++ frontend/src/components/Carnet/CarnetPreview.js
@@ -5,16 +5,23 @@ const CarnetPreview = () => {
     return ( 
         <>
             <Navbar />
-            <h2 className="text-center mt-5">Generar Carnet</h2>
+            <h2 className="text-center m-2">Generar Carnet</h2>
 
             <div className="row">
-                <div className="col-4">
+                <div className="col-4 d-flex flex-column align-items-center">
                     Vista previa del carnet
+                    <div className="w-75 border mt-2" style={{height: "400px"}}></div>
                 </div>
-                <div className="col-8 d-flex flex-colum align-items-center">
+                <div className="col-8 d-flex flex-column align-items-center justify-content-center">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis voluptatibus numquam, quaerat pariatur, rerum ratione perferendis assumenda illum non dolores quos qui facilis magnam maxime laborum est debitis commodi amet.
-                    <button>Descargar Carnet</button>
-                    <a href="mailto:gmail.com">Enviar Carnet</a>
+                    <div className="row mt-3 w-100">
+                        <div className="col-5">
+                            <button className="btn btn-dark btn-block redondeado">Descargar Carnet</button>
+                        </div>
+                        <div className="col-5">
+                            <a href="mailto:gmail.com" className="btn btn-dark btn-block redondeado">Enviar Carnet</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
